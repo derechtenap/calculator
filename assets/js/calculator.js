55dc6cf6655d974edd4467d45f253bef8027a5b9
Tim Deres <50623315+derechtenap@users.noreply.github.com>
2023-03-14 00:33:32 +0100

    Removed old assets

--- assets/js/calculator.js
+++
@@ -1,8 +0,0 @@
-const BUTTON = document.querySelectorAll('button');
-
-addEventListener('click', function (e) {
-    // Ignore onClick between buttons...
-    if (e.target.nodeName === 'BUTTON') {
-        console.info(e.target.className);
-    }
-});
