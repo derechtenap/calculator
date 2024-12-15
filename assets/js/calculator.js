ac85759b2059f67cfca4db24645a156c7d8da041
Tim Deres <50623315+derechtenap@users.noreply.github.com>
2024-12-15 15:24:07 +0100

    Add data attributes to identify each button

--- assets/js/calculator.js
+++ assets/js/calculator.js
@@ -1,8 +1,28 @@
 const BUTTONS = document.querySelectorAll('button');
 
-addEventListener('click', function (e) {
-    // Ignore onClick between buttons...
+addEventListener('click', (e) => {
+
+    // Ignore clicks between buttons...
     if (e.target.nodeName === 'BUTTON') {
-        console.info(e);
+
+        const BUTTON_TYPE = e.target.dataset.type
+
+        // Check for button type
+        if (BUTTON_TYPE === "number") {
+
+            console.info("It seems to be a number", e.target.dataset)
+        }
+
+        if (BUTTON_TYPE === "operator") {
+            console.info("It seems to be a operator", e.target.dataset)
+        }
+
+        if (BUTTON_TYPE === "equals") {
+            console.info("It seems to be equals", e.target.dataset)
+        }
+
+        if (BUTTON_TYPE === "clear") {
+            console.info("It seems to be clear", e.target.dataset)
+        }
     }
 });
